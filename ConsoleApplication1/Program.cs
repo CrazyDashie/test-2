b366e6a1917ac6bf039639d6add1abcdbd65f05d
DASHIE <DASHIE@DESKTOP-N402VB3>
2020-12-18 01:48:07 +0300

    New comment 2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("GitHubFromTest");
-            Console.WriteLine("GitHub2");//new
+            Console.WriteLine("GitHub2");//new2
         }
     }
 }

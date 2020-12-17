8f88d5a47a218b69f74497e829a0cee7bb862b04
DASHIE <DASHIE@DESKTOP-N402VB3>
2020-12-18 01:54:54 +0300

    3

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("GitHubFromTes");//new
+            Console.WriteLine("GitHubFromTe");//new
             Console.WriteLine("GitHub2");//new2
         }
     }

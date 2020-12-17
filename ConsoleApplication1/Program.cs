345793268f35a9391a949cf8afaa1c95c59c36cf
CrazyDashie <60315935+CrazyDashie@users.noreply.github.com>
2020-12-18 01:53:44 +0300

    2
    2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("GitHubFromTes");//new
+            Console.WriteLine("GitHubFromTest");//new
             Console.WriteLine("GitHub2");//new2
         }
     }
